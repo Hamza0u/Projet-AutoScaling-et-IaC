c251f3e0539759e385654a7125b52b497153cdb3
Hamza0u <aka.hamza02@gmail.com>
2025-04-01 17:29:35 +0200

    Ajout du script d'automatisation du déploiement Kubernetes

--- Kubernetes/Start.sh
+++ Kubernetes/Start.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
-"create deployments"
+echo "🚀 Démarrage de Minikube..."
+minikube start
 
-
-
-echo "Démarrage de Minikube..."
-minikube start --driver=docker &
-
-echo "Lancement du tunnel..."
+echo "🛣️ Lancement du tunnel..."
 minikube tunnel &
 
-"lancer le script pour update iP & "
+# Attendre quelques secondes pour éviter tout problème avec le tunnel
+sleep 10
 
-echo "Lancement du dashboard Kubernetes..."
+echo "📦 Création des déploiements..."
+kubectl apply -f redis-deployment.yml
+kubectl apply -f front-deployment.yml
+kubectl apply -f frontend-configmap.yml
+kubectl apply -f serveur-deployment.yml
+kubectl apply -f redis-replica-deployment.yml
+
+# Attendre que les services soient bien créés avant de mettre à jour le ConfigMap
+sleep 10
+
+echo "🔄 Lancement du script pour récupérer l'IP du serveur..."
+./update-configmap.sh
+
+echo "📊 Lancement du dashboard Kubernetes..."
 minikube dashboard &
+
